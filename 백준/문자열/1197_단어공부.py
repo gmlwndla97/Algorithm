3c7a9b0a648e78fe171f777858b1a8867b3541ea
gmlwndla97 <juju5471@naver.com>
2020-08-08 00:16:33 +0900

    PS

--- 백준/문자열/1197_단어공부.py
+++ 백준/문자열/1197_단어공부.py
@@ -1,7 +1,7 @@
 dic={}
 string=input()
 string=string.upper()
-check=''.join(set(string))
+check=''.join(set(string)) #문자열 중복제거
 max=0;
 count=0;
 for i in range(0, len(check)):
@@ -11,8 +11,8 @@ for i in range(0, len(check)):
             max=count
         count=0
 
-sorted(dic.items(), key=lambda x:x[1], reverse=True)
-rev={v:k for k, v in dic.items()}
+sorted(dic.items(), key=lambda x:x[1], reverse=True) #value값 기준으로 내림차순 정렬
+rev={v:k for k, v in dic.items()}  #key, value 순서 반대로 한 dictionary
 #print(dic)
 duplicatenum=0
 for k in dic.values():
