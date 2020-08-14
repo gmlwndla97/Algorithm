b0736e0d8a8295eeb3ab0236349eb4bab7d52b0d
gmlwndla97 <juju5471@naver.com>
2020-08-14 22:15:40 +0900

    sort

---
+++ 백준/정렬/2750_수정렬하기.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+list = []
+min = 987654321
+for i in range(0, n):
+    x = int(input())
+    list.append(x)
+
+index = 0
+for i in range(0, n - 1):
+    for j in range(i, n):
+        if list[j] < min:
+            min = list[j]
+            index = j
+    temp = list[i]
+    list[i] = min
+    list[index] = temp
+    min = 987654321
+
+for i in list:
+    print(i)
