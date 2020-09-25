d66ff2a672395dacb962cfc0c114093d708c71c5
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2020-09-25 18:52:45 +0900

    priority queue

--- 백준/정렬/2751_수정렬하기2.py
+++ 백준/정렬/2751_수정렬하기2.py
@@ -1,34 +1,11 @@
 import sys
 #############백준 시간초과 해결안됨
-def merge(left, right):
-    result = []
-    while len(left) > 0 or len(right) > 0:
-        if len(left) > 0 and len(right) > 0:
-            if left[0] < right[0]:
-                result.append(left[0])
-                left = left[1:]
-            else:
-                result.append(right[0])
-                right = right[1:]
+from queue import PriorityQueue
+q=PriorityQueue()
 
-        elif len(left) > 0: #오른쪽 배열이 없는 경우
-            result.append(left[0])
-            left = left[1:]
-        elif len(right) > 0: #왼쪽 배열이 없는 경우
-            result.append(right[0])
-            right = right[1:]
-    return result
+n=int(sys.stdin.readline().rstrip())
+for i in range(0, n):
+    q.put(int(sys.stdin.readline().rstrip()))
 
-
-def mergesort(array):
-    if len(array)<=1:
-        return array
-    mid=len(array)//2
-    left=mergesort(array[:mid])
-    right=mergesort(array[mid:])
-    return merge(left, right)
-
-
-list =[int(sys.stdin.readline()) for i in range(int(sys.stdin.readline()))]
-result=mergesort(list)
-print(*result,sep='\n')
+while q.qsize():
+    print(q.get())
