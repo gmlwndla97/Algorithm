b6c513dd41f841edbb4a3a01dff4caab04a6ff04
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2021-01-17 01:03:42 +0900

    자료구조

--- 백준/자료구조/1158_요세푸스문제.py
+++ 백준/자료구조/1158_요세푸스문제.py
@@ -1,28 +1,14 @@
 import sys
 n, k=map(int, sys.stdin.readline().rstrip().split())
 answer=[]
-for i in range(1, n+1):
-    answer.append(i)
-
-def resize(now, len):
-    while now <= len:
-        if now<0:
-            now+=len
-            break
-
-    return now
-
-now=k-1
-result=[]
+temp=[]
 for i in range(0, n):
-    print(now)
- #   print(answer[now])
-    result.append(answer[now])
-    answer.remove(answer[now])
+    answer.append(i+1)
 
-    now=now+k-1
-    if now>len(answer):
-        now=resize(now, len(answer))
-
-
-print(result)
+num=0
+for i in range(0, n):
+    num += k-1
+    if num>=len(answer):
+        num= num%len(answer)
+    temp.append(str(answer.pop(num)))
+print("<",", ".join(temp)[:], ">", sep='')
