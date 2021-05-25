7bdba3f9921acdbefb3560d067e04e909796c20d
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2021-05-26 01:48:42 +0900

    bfs

---
+++ 백준/DFS와BFS/18352_특정거리의도시찾기.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+q=deque()
+n, m, k, x=map(int,sys.stdin.readline().rstrip().split())
+graph=[[] for _ in range(n+1)]
+visit=[0]*(n+1)
+for _ in range(m):
+    a, b=map(int,sys.stdin.readline().rstrip().split())
+    graph[a].append(b)
+
+q.append((x, 0))
+answer=[]
+while(q):
+    node, depth=q[0]
+    if(depth==k):
+        answer.append(node)
+    q.popleft()
+    for i in graph[node]:
+        if(visit[i]==0):
+            visit[i]=1
+            q.append((i, depth+1))
+
+if(len(answer)==0):
+    print(-1)
+    sys.exit(0)
+for i in answer:
+    print(i)
