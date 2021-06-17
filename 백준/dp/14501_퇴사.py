14ee2597f4dfd56360125faf559c27725dc882ef
gmlwndla97 <37864097+gmlwndla97@users.noreply.github.com>
2021-06-17 22:11:10 +0900

    dp

--- 백준/dp/14501_퇴사.py
+++ 백준/dp/14501_퇴사.py
@@ -2,17 +2,19 @@ import sys
 n=int(input())
 tt=[]
 pp=[]
+dp=[0]*(n+1)
 for _ in range(n):
     t, p=map(int,sys.stdin.readline().rstrip().split())
     tt.append(t)
     pp.append(p)
 
 maxval=0
-dp=[0]*n
 for i in range(n-1, -1, -1):
-    if(i+tt[i]>=n):
-        dp[i]=dp[i+1]
+    if(i+tt[i]<=n):
+        dp[i]=max(maxval, pp[i]+dp[i+tt[i]])
     else:
-        dp[i]=max(dp[i+1], dp[i+tt[i]]+p[i])
+        dp[i]=dp[i+1]
+    if(dp[i]>maxval):
+        maxval=dp[i]
 
-print(max(dp))
+print(maxval)
